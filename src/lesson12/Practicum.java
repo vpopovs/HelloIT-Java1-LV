94cc747594ba430539eac4547159424399e24f99
V. Popovs <57049218+vpopovs@users.noreply.github.com>
2024-01-06 17:36:37 +0200

    Update Practicum.java

--- src/lesson12/Practicum.java
+++ src/lesson12/Practicum.java
@@ -3,7 +3,7 @@ package lesson12;
 public class Practicum {
 
     /*
-    Izveidot bankas akaunta klasi.
+    Izveidot bankas konta klasi.
     Katram akauntam jāsatur īpašnieka vārds un sākotnējā bilance. 
     Jābūt iespējai izņemt naudu no akaunta vai ielikt to, 
     iegūt īpašnieka vārdu un tekošo bilanci.
